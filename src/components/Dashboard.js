917889e2ce769876b97ef82828410a253469a060
erikvs <89622099+erikvs@users.noreply.github.com>
2023-05-14 10:41:09 +0200

    Update Dashboard.js
    - Changed shopgames on dashboard to display different type of game and correct amount according to assignment.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,12 +5,20 @@ import ShopCard from './ShopCard';
 
 function Dashboard() {
 
-  const [games, setGames] = useState([]);
+  const [myGames, setMyGames] = useState([]);
+  const [shopGames, setShopGames] = useState([]);
 
   useEffect(() => {
     fetch('https://api.rawg.io/api/games?key=58f53988e4e5435abcf085d18cffb058&genres=adventure&page_size=4')
       .then(response => response.json())
-      .then(data => setGames(data.results))
+      .then(data => setMyGames(data.results))
+      .catch(error => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    fetch('https://api.rawg.io/api/games?key=58f53988e4e5435abcf085d18cffb058&genres=racing&page_size=3')
+      .then(response => response.json())
+      .then(data => setShopGames(data.results))
       .catch(error => console.log(error));
   }, []);
 
@@ -20,13 +28,13 @@ function Dashboard() {
       <p>Welcome to the dashboard!</p>
       <section className='MygamesGames'>
         <h2>My Games-Library</h2>
-      {games.map(game => <GameCard key={game.id} game={game} />)}
+      {myGames.map(game => <GameCard key={game.id} game={game} />)}
       <Link to="/my-games"><button>Go to My Games</button></Link>
       </section>
       
       <section className='MygamesGames'>
         <h2>Game Shop</h2>
-      {games.map(game => <ShopCard key={game.id} game={game} />)}
+      {shopGames.map(game => <ShopCard key={game.id} game={game} />)}
       <Link to="/GameShop"><button>Go to Game Shop</button></Link>
       </section>
     </div>
@@ -34,6 +42,3 @@ function Dashboard() {
 }
 
 export default Dashboard;
-
-
-//TODO: This project made possible by using https://rawg.io/apidocs - attribution-link to API
