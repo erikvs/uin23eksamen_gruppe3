a0bf4774ab50261afbb076c8b10692c65c3db5c1
erikvs <89622099+erikvs@users.noreply.github.com>
2023-05-14 20:52:48 +0200

    Update ShopCard.js
    Added "buy button" functionality from ShopCard

--- src/components/ShopCard.js
+++ src/components/ShopCard.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import placeHolderImage from "../resources/placeHolderImage.avif"
 
 function ShopCard ({ game }) {
+  const [website, setWebsite] = useState('');
+
+  useEffect(() => {
+    const fetchGameWebsite = () => {
+      fetch(`https://api.rawg.io/api/games/${game.id}?key=58f53988e4e5435abcf085d18cffb058`)
+        .then(response => response.json())
+        .then(data => {
+          setWebsite(data.website);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    };
+
+    fetchGameWebsite();
+  }, [game.id]);
+
+  const handleVisitWebsite = () => {
+    window.open(website, '_blank');
+  };
+
   return (
 <div className='MygamesGames'>
 <article >
@@ -13,7 +35,11 @@ function ShopCard ({ game }) {
 </Link>
 <h3>{game.name}</h3>
 <p>{game.genres.map(genre => genre.name).join(', ')}</p>
-<button href={game.website}>Buy</button>
+{website ? (
+          <button onClick={handleVisitWebsite}>Buy</button>
+        ) : (
+          <p>No website available</p>
+        )}
 </article>
 </div>
 );
