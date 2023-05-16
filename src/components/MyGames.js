3f500309b8f6eaf7096cc4690db8303e23f2fa38
erikvs <89622099+erikvs@users.noreply.github.com>
2023-05-16 15:47:37 +0200

    Updated MyGames
    - cleaned imports
    - made API async
    - removed one empty line from ShopCard

--- src/components/MyGames.js
+++ src/components/MyGames.js
@@ -1,16 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import GameCard from './GameCard'
 
-
 function MyGames() {
-
   const [games, setGames] = useState([]);
 
   useEffect(() => {
-    fetch('https://api.rawg.io/api/games?key=58f53988e4e5435abcf085d18cffb058&genres=adventure&page_size=20')
-      .then(response => response.json())
-      .then(data => setGames(data.results))
-      .catch(error => console.log(error));
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.rawg.io/api/games?key=58f53988e4e5435abcf085d18cffb058&genres=adventure&page_size=20');
+        const data = await response.json();
+        setGames(data.results); 
+      } 
+      catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
   }, []);
 
   return (
